45c181e85249c2e617e0908effb7d7616db629c0
David Vanderson <david.vanderson@gmail.com>
2013-12-06 13:19:03 -0500

    use macro to preserve dc transform

--- draw-intro.rkt
+++ draw-intro.rkt
@@ -9,11 +9,11 @@
 
 
 (define (draw-intro dc)
-  (define t (send dc get-transformation))
-
-  (send dc scale 1 -1)
-  (send dc draw-text "Intro Screen" 0 0)
-  
-  ; This screen is where you type in your name and the server IP
-  
-  (send dc set-transformation t))
+  (keep-transform dc
+    
+    (send dc scale 1 -1)
+    (send dc draw-text "Intro Screen" 0 0)
+    
+    ; This screen is where you type in your name and the server IP
+    
+    ))
--- draw.rkt
+++ draw.rkt
@@ -8,7 +8,7 @@
 
 (provide (all-defined-out))
 
-(define-syntax-rule (with-transform dc e ...)
+(define-syntax-rule (keep-transform dc e ...)
   (begin
     (define t (send dc get-transformation))
     e ...
@@ -20,14 +20,13 @@
 
 (define (draw-framerate dc frames)
   (when (not (empty? frames))
-    (define t (send dc get-transformation))
-    (send dc translate (- (/ WIDTH 2)) (/ HEIGHT 2))
-    (send dc scale 1 -1)
-    (define start (list-ref frames (- (length frames) 1)))
-    (define end (first frames))
-    (define span (/ (- end start) 1000))
-    (send dc draw-text (format "~a" (truncate (/ (- (length frames) 1) span))) 0 0 #t)
-    (send dc set-transformation t)))
+    (keep-transform dc
+      (send dc translate (- (/ WIDTH 2)) (/ HEIGHT 2))
+      (send dc scale 1 -1)
+      (define start (list-ref frames (- (length frames) 1)))
+      (define end (first frames))
+      (define span (/ (- end start) 1000))
+      (send dc draw-text (format "~a" (truncate (/ (- (length frames) 1) span))) 0 0 #t))))
 
 
 (define (recenter o x y)
@@ -40,56 +39,48 @@
 
 
 (define (draw-shield dc shield)
-  (define t (send dc get-transformation))
-  
-  (define num (vector-length (shield-sections shield)))
-  (define radius (shield-radius shield))
-  
-  (for ((section (shield-sections shield))
-        (i (in-naturals))
-        #:when (section . >= . 1))
-    (define linear-strength (/ section (shield-max shield)))
-    (define log-strength (/ (log (add1 section)) (log (shield-max shield))))
-    (send dc set-pen (shield-color shield) (* 3 (* 0.5 (+ linear-strength log-strength))) 'solid)
-    (define r (- 2pi (/ (* 2pi i) num)))
-    (define arc-size (* log-strength (/ 2pi num)))
-    (send dc draw-arc
-          (- radius) (- radius)
-          (* 2 radius) (* 2 radius)
-          (- r (* 0.5 arc-size)) (+ r (* 0.5 arc-size))))
-  
-  (send dc set-transformation t))
+  (keep-transform dc
+    (define num (vector-length (shield-sections shield)))
+    (define radius (shield-radius shield))
+    
+    (for ((section (shield-sections shield))
+          (i (in-naturals))
+          #:when (section . >= . 1))
+      (define linear-strength (/ section (shield-max shield)))
+      (define log-strength (/ (log (add1 section)) (log (shield-max shield))))
+      (send dc set-pen (shield-color shield) (* 3 (* 0.5 (+ linear-strength log-strength))) 'solid)
+      (define r (- 2pi (/ (* 2pi i) num)))
+      (define arc-size (* log-strength (/ 2pi num)))
+      (send dc draw-arc
+            (- radius) (- radius)
+            (* 2 radius) (* 2 radius)
+            (- r (* 0.5 arc-size)) (+ r (* 0.5 arc-size))))))
 
 
 (define (draw-ship dc s center)
-  (define t (send dc get-transformation))
-  
-  (define posvel (obj-posvel s))
-  (define-values (x y) (recenter center (posvel-x posvel) (posvel-y posvel)))
-  (send dc translate x y)
-  
-  (define t2 (send dc get-transformation))
-  (send dc rotate (- (posvel-r posvel)))
-  (for ((shield (ship-shields s)))
-    (draw-shield dc shield))
-  
-  (send dc set-pen "black" 1 'solid)
-  (send dc draw-polygon '((10 . 10)
-                          (20 . 0)
-                          (10 . -10)
-                          (-10 . -10)
-                          (-10 . 10)))
-  
-  (send dc set-transformation t2)
-  
-  (define scale 0.5)
-  (send dc scale scale (- scale))
-  (define text (~r (* 100 (ship-containment s)) #:precision 0))
-  (define-values (w h b v) (send dc get-text-extent text))
-  (send dc translate (* -0.5 w) (* -0.5 h))
-  (send dc draw-text text 0 0)
-  
-  (send dc set-transformation t))
+  (keep-transform dc
+    (define posvel (obj-posvel s))
+    (define-values (x y) (recenter center (posvel-x posvel) (posvel-y posvel)))
+    (send dc translate x y)
+    
+    (keep-transform dc
+      (send dc rotate (- (posvel-r posvel)))
+      (for ((shield (ship-shields s)))
+        (draw-shield dc shield))
+      
+      (send dc set-pen "black" 1 'solid)
+      (send dc draw-polygon '((10 . 10)
+                              (20 . 0)
+                              (10 . -10)
+                              (-10 . -10)
+                              (-10 . 10))))
+    
+    (define scale 0.5)
+    (send dc scale scale (- scale))
+    (define text (~r (* 100 (ship-containment s)) #:precision 0))
+    (define-values (w h b v) (send dc get-text-extent text))
+    (send dc translate (* -0.5 w) (* -0.5 h))
+    (send dc draw-text text 0 0)))
 
 
 (define (draw-plasma dc p center)
@@ -100,7 +91,7 @@
 
 
 (define (draw-no-role dc ownspace)
-  (define t (send dc get-transformation))
+  (keep-transform dc
   
   (define max-x (space-sizex ownspace))
   (define max-y (space-sizey ownspace))
@@ -113,12 +104,7 @@
       ((ship? o)
        (draw-ship dc o center))
       ((plasma? o)
-       (draw-plasma dc o center))))
-  
-  (send dc scale 1 -1)
-  (send dc draw-text "hello" 0 0)
-  
-  (send dc set-transformation t))
+       (draw-plasma dc o center))))))
 
 
 (define (draw-playing dc ownspace stack)
